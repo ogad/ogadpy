52d533306f8730eff76c1e4be3b7f6f9c33f7f1a
Oliver Driver <oliver.ga.driver@gmail.com>
2023-11-17 13:33:26 +0000

    Many untracked changes

--- src/ogadpy/plotting/dataset_plot.py
+++ src/ogadpy/plotting/dataset_plot.py
@@ -1,14 +1,12 @@
 from abc import ABC, abstractmethod
 from inspect import isgenerator
 from itertools import product
-import stat
 from matplotlib.pylab import f
-from numpy import fix
 import xarray as xr
 import matplotlib.pyplot as plt
-import seaborn as sns
 import logging
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import numpy as np
 
 
 class DatasetPlot(ABC):
@@ -19,30 +17,47 @@ class DatasetPlot(ABC):
         plot_var = plot_var if plot_var is not None else self.default_var()
 
         dim_kwargs = {k: v for k, v in kwargs.items() if k in self.dataset.dims}
+        kwargs = {k: v for k, v in kwargs.items() if k not in self.dataset.dims}
         to_plot = self.dataset[plot_var].sel(**dim_kwargs)
 
-        remaining_dims = self.remaining_dims(dims)
+        remaining_dims = self.remaining_dims(dims + tuple(dim_kwargs.keys()))
 
         # If axis/axes are specified, use them by passing them to _plot as a generator
-        ax = kwargs.pop("ax", None)
-        if ax is not None:
+        axes = kwargs.pop("ax", None)
+        if axes is not None:
             try:
-                ax = iter(ax)
+                axes = iter(axes)
             except TypeError:
-                ax = iter([ax])
-            kwargs["ax"] = ax
+                axes = iter([axes])
+        else:
+            axes = None
 
         plot_points = list(
             product(*[self.dataset[dim].values for dim in remaining_dims])
         )
         if len(plot_points) > 1:
             logging.info(f"Plotting {len(plot_points)} plots")
+
+        plot_returns = []
         for point in plot_points:
             fixed_vars = {dim: val for dim, val in zip(remaining_dims, point)}
-            plt.subplots()
+            if axes is None:
+                _, ax = plt.subplots()
+            else:
+                ax = next(axes)
             data_at_point = to_plot.sel(**fixed_vars)
-            self._plot(*dims, data_at_point, **kwargs)
-            self.set_title(fixed_vars)
+            plot_returns.append(self._plot(*dims, data_at_point, ax=ax, **kwargs))
+            self.set_title(fixed_vars, ax)
+
+        return_val = []
+        for i in range(len(plot_returns[0])):
+            return_val.append([plot_return[i] for plot_return in plot_returns])
+
+        return tuple(return_val)
+
+    @abstractmethod
+    def _plot(self, *dims, plot_data, ax, **kwargs) -> tuple[plt.Axes, ...]:
+        ...
 
     def default_var(self):
         var = [i for i in self.dataset.data_vars][0]
@@ -60,13 +75,11 @@ class DatasetPlot(ABC):
 
         return dims
 
-    def label_axes(self, x_var, y_var):
-        ax = plt.gca()
+    def label_axes(self, x_var, y_var, ax):
         ax.set_xlabel(x_var)
         ax.set_ylabel(y_var)
 
-    def set_title(self, fixed_vars: dict):
-        ax = plt.gca()
+    def set_title(self, fixed_vars: dict, ax):
         title = ", ".join([f"{k}: {v}" for k, v in fixed_vars.items()])
         ax.set_title(title)
 
@@ -74,12 +87,7 @@ class DatasetPlot(ABC):
 class DatasetPlot2D(DatasetPlot):
     """Wrapper for plt.pcolormesh"""
 
-    def _plot(self, x_dim, y_dim, plot_data, **kwargs):
-        ax = kwargs.pop("ax", plt.gca())
-
-        if isgenerator(ax):
-            ax = next(ax)
-
+    def _plot(self, x_dim, y_dim, plot_data, ax, **kwargs):
         im = ax.pcolormesh(
             self.dataset[x_dim],
             self.dataset[y_dim],
@@ -87,9 +95,10 @@ class DatasetPlot2D(DatasetPlot):
             **kwargs,
         )
 
-        self.label_axes(x_dim, y_dim)
+        self.label_axes(x_dim, y_dim, ax)
         d = make_axes_locatable(ax)
         cax = d.append_axes("right", size="5%", pad=0.05)
-        plt.colorbar(im, cax=cax)
+        cb = plt.colorbar(im, cax=cax)
         cax.set_ylabel(plot_data.name)
-        plt.axes(ax)
+
+        return (ax, cax)
--- src/ogadpy/plotting/outlines.py
+++ src/ogadpy/plotting/outlines.py
@@ -1,43 +1,44 @@
 # from https://stackoverflow.com/questions/24539296/outline-a-region-in-a-graph
 
-def outline_binary(mapimg, extent=None, ax=None):
+
+def outline_binary(mapimg, extent=None, ax=None, **kwargs):
     import numpy as np
     import matplotlib.pyplot as plt
-    
+
     if not mapimg.any():
         return
-    
+
     if ax is None:
         ax = plt.gca()
-    
+
     if extent is None:
         try:
             extent = ax.images[0].get_extent()
         except IndexError:
-            extent = (-0.5, mapimg.shape[1]-0.5, -0.5, mapimg.shape[0]-0.5)
+            extent = (-0.5, mapimg.shape[1] - 0.5, -0.5, mapimg.shape[0] - 0.5)
     x0, x1, y0, y1 = extent
-    
+
     # a vertical line segment is needed, when the pixels next to each other horizontally
     #   belong to diffferent groups (one is part of the mask, the other isn't)
-    # after this ver_seg has two arrays, one for row coordinates, the other for column coordinates 
-    ver_seg = np.where(mapimg[:,1:] != mapimg[:,:-1])
+    # after this ver_seg has two arrays, one for row coordinates, the other for column coordinates
+    ver_seg = np.where(mapimg[:, 1:] != mapimg[:, :-1])
 
     # the same is repeated for horizontal segments
-    hor_seg = np.where(mapimg[1:,:] != mapimg[:-1,:])
+    hor_seg = np.where(mapimg[1:, :] != mapimg[:-1, :])
 
     # if we have a horizontal segment at 7,2, it means that it must be drawn between pixels
     #   (2,7) and (2,8), i.e. from (2,8)..(3,8)
     # in order to draw a discountinuous line, we add Nones in between segments
     l = []
     for p in zip(*hor_seg):
-        l.append((p[1], p[0]+1))
-        l.append((p[1]+1, p[0]+1))
-        l.append((np.nan,np.nan))
+        l.append((p[1], p[0] + 1))
+        l.append((p[1] + 1, p[0] + 1))
+        l.append((np.nan, np.nan))
 
     # and the same for vertical segments
     for p in zip(*ver_seg):
-        l.append((p[1]+1, p[0]))
-        l.append((p[1]+1, p[0]+1))
+        l.append((p[1] + 1, p[0]))
+        l.append((p[1] + 1, p[0] + 1))
         l.append((np.nan, np.nan))
 
     # now we transform the list into a numpy array of Nx2 shape
@@ -47,8 +48,12 @@ def outline_binary(mapimg, extent=None, ax=None):
     #   at this point let's assume it has extents (x0, y0)..(x1,y1) on the axis
     #   drawn with origin='lower'
     # with this information we can rescale our points
-    segments[:,0] = x0 + (x1-x0) * segments[:,0] / mapimg.shape[1]
-    segments[:,1] = y0 + (y1-y0) * segments[:,1] / mapimg.shape[0]
+    segments[:, 0] = x0 + (x1 - x0) * segments[:, 0] / mapimg.shape[1]
+    segments[:, 1] = y0 + (y1 - y0) * segments[:, 1] / mapimg.shape[0]
 
     # and now there isn't anything else to do than plot it
-    ax.plot(segments[:,0], segments[:,1], color=(1,0,0,.5), linewidth=3)
+    if "color" not in kwargs:
+        kwargs["color"] = (1, 0, 0, 0.5)
+    if "linewidth" not in kwargs:
+        kwargs["linewidth"] = 3
+    ax.plot(segments[:, 0], segments[:, 1], **kwargs)
